a2f88934bb2710619ac2784113b6327d6aa1ad8b
min-elena-k <dev.elena.k@gmail.com>
2021-01-22 01:56:05 +0900

    all i need to know how about state - for me setState(current => {})

--- src/App.js
+++ src/App.js
@@ -1,72 +1,30 @@
-import React from 'react';
-import PropTypes from 'prop-types'
+import React, { Component } from 'react';
 
-function Food({ name, image, rating }) { 
-  return <div>
-    <h2>I like {name}</h2>
-    <h4> rating {rating}/5.0</h4>
-    <img src={image} alt={name}/>
-  </div>
-}
+class App extends Component {
+  state = {
+    count: 0
+  };
 
-// isRequired 없으면, 지정한 타입 또는 undefined
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  image: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired
-}
+  add = () => {
+    this.setState(current => ({
+      count: current.count + 1
+    }))
+  };
+  minus = () => {
+    this.setState(current => ({
+      count: current.count - 1
+    }))
+  };
 
-// 이미지 출처 김가네
-const foodILike = [
-  {
-    id: 1,
-    name: "Kimbap",
-    image: "http://gimgane.co.kr/upload/menu_01/2019_10_07/hero_dnA3c_2019_10_07_19_21_49.jpg",
-    rating: 5
-  },
-  {
-    id: 2,
-    name: "OmeltetRice",
-    image: "http://gimgane.co.kr/upload/menu_01/2018_09_21/hero_sUMOk_2018_09_21_04_12_30.jpg",
-    rating: 4.6
-  },
-  {
-    id: 3,
-    name: "KimchiStew",
-    image: "http://gimgane.co.kr/upload/menu_01/2018_09_21/hero_1s9Qk_2018_09_21_03_59_13.jpg",
-    rating: 3.2
-  },
-  {
-    id: 4,
-    name: "Ramen",
-    image: "http://gimgane.co.kr/upload/menu_01/2018_09_21/hero_Kxrd8_2018_09_21_12_52_20.jpg",
-    rating: 4.1
-  },
-  {
-    id: 5,
-    name: "Dumpling",
-    image: "http://gimgane.co.kr/upload/menu_01/2018_09_21/hero_y19Hp_2018_09_21_03_52_34.jpg",
-    rating: 2.8
+  render() {
+    return (
+      <div>
+        <h1>The number is: {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
   }
-]
-
-function renderFood(dish) {
-  console.log(dish);
-  return <Food name={dish.name} image={dish.image} rating={dish.rating}/>
-}
-
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello</h1>
-            
-      {/* map 사용하기 1 */}
-      {foodILike.map(dish => <Food key={dish.id} name={dish.name} image={dish.image} rating={dish.rating}/>)}
-      
-      {/* map 사용하기 2 */}
-      {/* {foodILike.map(renderFood)} */}
-    </div>
-  );
 }
 
 export default App;
